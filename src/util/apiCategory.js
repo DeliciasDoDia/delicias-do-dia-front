38c3233e3c9378ab28b7402bc9babf895ffff412
Geovanna Lima <geovannadasilvalima@gmail.com>
2024-11-23 19:04:09 -0300

    fix: mudando de lugar

---
+++ src/util/apiCategory.js
@@ -0,0 +1,21 @@
+// ----- READ -----
+export async function getCategories() {
+  try {
+    const response = await fetch("http://localhost:8080/api/categories");
+    const data = await response.json();
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.log("ERROR: " + error);
+  }
+}
+
+export async function getCategoryById(id) {
+  try {
+    const response = await fetch("http://localhost:8080/api/categories/" + id);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("ERROR: " + error);
+  }
+}
