fb960ac667e8160864c40bc29561066035b35654
Pedro Fernandes <pedrovosvisk10@gmail.com>
2024-11-23 14:53:01 -0300

    fix: ajusta logica de imagens da categoria

--- portal-receitas/next.config.mjs
+++ portal-receitas/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['drive.usercontent.google.com', 'drive.google.com'],
+  },
+};
 
 export default nextConfig;
+
--- portal-receitas/src/app/components/Menu.jsx
+++ portal-receitas/src/app/components/Menu.jsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
-export default function Menu({ name }) {
+export default function Menu({ name, iconUrl}) {
   return (
     <button type="button" className="text-black bg-transparent gap-5 border border-gray hover:bg-yellow font-medium rounded-full text-sm px-2 py-2.5 text-center inline-flex items-center me-2 mb-2 w-64">
       <div className='flex content-center justify-center items-center bg-white rounded-full p-1 h-10 w-10'>
-        <Image src="/bolos.png" alt="" width={24} height={24} />
+        <Image src={iconUrl} alt="" width={24} height={24} />
       </div>
       {name}
     </button>
