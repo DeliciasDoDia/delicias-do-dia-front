6bb1aca320c982c9ff9182df4ae0dc54d362ad72
Geovanna Lima <geovannadasilvalima@gmail.com>
2024-11-24 20:29:18 -0300

    fix: ajuste no modal de sucesso

---
+++ src/app/components/UserSettingsModal.jsx
@@ -0,0 +1,7 @@
+export default function UserSettingsModal() {
+	return (
+		<div className="m-4">
+			<img className="rounded-lg w-full object-cover mb-8 md:mb-16" src="/banner.png" alt="" />
+		</div>
+	);
+}
